e138146901f6323562f7564fb2491dbc4f3dfc55
parimal5 <parimalmeshram15@gmail.com>
2021-08-05 19:44:48 +0530

    Added Two Paddle and get it to move up and down

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from turtle import Turtle, Screen
-
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
+
 screen.bgcolor("black")
 screen.setup(height=600, width=800)
-screen.title("The Arcade Game")
+screen.title("Pong")
+screen.tracer(0)
 
-tim = Turtle()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
