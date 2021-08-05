86276bbf9213a422db3c174203d8fa35ea1afcd3
parimal5 <parimalmeshram15@gmail.com>
2021-08-05 18:58:39 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("The Arcade Game")
+
+tim = Turtle()
+
+
+
+screen.exitonclick()
